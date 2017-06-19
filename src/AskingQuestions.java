4bb07036ffd371bc110c0057308f96ddf0927b82
Taimur <tsohrab8@gmail.com>
2017-06-19 14:23:13 -0400

    added change assignment

--- src/AskingQuestions.java
+++ src/AskingQuestions.java
@@ -4,6 +4,7 @@ public class AskingQuestions
 { 
   public static void main( String[] args ) 
   { 
+	// InputOutput Debug Assignment
     Scanner keyboard = new Scanner(System.in);
 
     int num1, num2, num3;
@@ -18,5 +19,15 @@ public class AskingQuestions
     num3 = keyboard.nextInt();
   
   	System.out.println("The total is : " + (num1 + num2 + num3));
+  	
+  	//Input Output Change Assignment
+
+    System.out.print( "First temperature? " );
+    num1 = keyboard.nextInt();
+
+    System.out.print( "Second temperature? " );
+    num2 = keyboard.nextInt();
+
+    System.out.println("The average value is " + ((num1 + num2)/2));
   }
 }
